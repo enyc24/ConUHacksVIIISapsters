72972ba92bad83c78344731f52d34e6f79b3c205
enyc24 <157230167+enyc24@users.noreply.github.com>
2024-01-20 15:47:46 -0500

    Create Car.java

---
+++ Car.java
@@ -0,0 +1,15 @@
+
+public class Car {
+	
+	final int compact = 0;
+	final int medium = 1;
+	final int fullSize = 2;
+	final int class1 = 3;
+	final int class2 = 4;
+	
+	int [] type = {0, 1, 2, 3, 4};
+	int [] serviceTime = {30, 30, 30, 60, 120};
+	int [] servicePrice = {150, 150, 150, 250, 700};	
+	
+	
+}
