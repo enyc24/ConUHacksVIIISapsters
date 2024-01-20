905023306d70908a8d130ab9c5b8cdae22446eff
jGavranovic <150096395+jGavranovic@users.noreply.github.com>
2024-01-20 15:20:01 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,27 @@
+import java.io.File;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.util.Calendar;
 
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new  File("C:\\Users\\jovro\\Desktop\\SAP\\datafile.csv");
+        Scanner scan = new Scanner(file);
+
+        Appointment[] appointments = new Appointment[10000];
+
+
+        for (int i=0; i<10000; i++){
+            String currentLine = scan.nextLine();
+            appointments[i] = new Appointment(currentLine.substring(0,currentLine.indexOf(",")),currentLine.substring(currentLine.indexOf(",")+1,currentLine.lastIndexOf(",")),currentLine.substring(currentLine.lastIndexOf(",")+1));
+        }
+
+
+        for (int i=0;i<10000;i++){
+            System.out.println(appointments[i]);
+        }
+
+
+        scan.close();
+    }
+}
