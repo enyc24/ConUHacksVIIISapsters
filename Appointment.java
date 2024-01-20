6a33b7d9b30ea36d89d925c637b923151ab14ed7
jGavranovic <150096395+jGavranovic@users.noreply.github.com>
2024-01-20 14:41:42 -0500

    Update and rename Timeslot.java to Appointment.java

---
+++ Appointment.java
@@ -0,0 +1,10 @@
+
+public class Appointment{
+    private Calendar appointmentRequestDate;
+    private Calendar appointmentDate;
+    private Car car;
+
+    public Appointment(String appointmentRequestDate, String apointmentDate, String car){
+        this.appointmentRequestDate = Calendar(appointmentRequestDate);
+    }
+}
